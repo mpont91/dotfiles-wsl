51595785a6cea59d37e984928d559d5a16f14ed6
Marc Pont <mpont91@gmail.com>
2025-05-20 09:27:08 +0200

    configure nvm to use .nvmrc of default version

--- terminal/nvm-config.zsh
+++ terminal/nvm-config.zsh
@@ -1,3 +1,9 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm use --silent >/dev/null 2>&1 # Tries to load npm version from .nvmrc file
+
+if ! command -v node >/dev/null 2>&1; then
+    nvm use default --silent >/dev/null 2>&1 # Load default version if there is not .nvmrc file
+fi
